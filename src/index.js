410cf0142f5d6139596be2dff19d314d118b7bf0
Kalen Collins <sensei@shivestudios.com>
2017-09-04 10:24:13 -0500

    Added functions to exectute HTTP request in alexa skill

--- src/index.js
+++ src/index.js
@@ -2,19 +2,57 @@
 var Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+   var alexa = Alexa.handler(event, context);
+   alexa.registerHandlers(handlers);
+   alexa.execute();
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello')
-    },
-    'SayHello': function () {
-        this.emit(':tell', 'Hello World!');
-    }
+   'LaunchRequest': function () {
+       this.emit('SayHello');
+   },
+   'HelloWorldIntent': function () {
+       this.emit('SayHello')
+   },
+   'SayHello': function () {
+       this.emit(':tell', 'Hello World!');
+   }
 };
+
+function handleGetInfoIntent(intent, session, callback) {
+
+   var speechOutput = "We have an error"
+
+   getJSON(function(data) {
+       if(data != "ERROR") {
+           var speechOutput = data;
+       }
+       callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput));
+   })
+}
+
+function url(data){
+   var url = "http://www.google.com/";
+
+   //handle concatonate data to url
+
+   return url;
+}
+
+function getJSON(callback) {
+
+   request.get(url(), function(err, res, body) {
+
+       var data = JSON.parse(body);
+
+       var travelTime = data.rows.elements.duration_in_traffic.text;
+
+       if(travelTime > 0) {
+           callback(travelTime);
+       } else {
+           callback("ERROR");
+       }
+   })
+
+
+}
